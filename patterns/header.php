77f0d1aafa85354ce66ed14e216b2c981a5dada4
gcsawnam <gcsawnam@gmail.com>
2023-03-09 14:17:43 +0545

    changes in header banner and some css section

--- patterns/header.php
+++ patterns/header.php
@@ -5,10 +5,13 @@
   * Categories:featured
   */
 ?>
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div id="overlay" class="wp-block-group"></div>
+<!-- /wp:group -->
 
 <!-- wp:group {"align":"wide","style":{"spacing":{"margin":{"top":"0","bottom":"0"},"padding":{"top":"0","right":"0","bottom":"0","left":"0"}}},"layout":{"type":"constrained","justifyContent":"center","contentSize":"1300px"}} -->
-<div class="wp-block-group alignwide" style="margin-top:0;margin-bottom:0;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:group {"style":{"spacing":{"padding":{"top":"0","right":"0","bottom":"0","left":"0"}}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
-<div class="wp-block-group" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group alignwide" style="margin-top:0;margin-bottom:0;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:group {"style":{"spacing":{"padding":{"top":"0","right":"0","bottom":"0","left":"0"}}},"className":"Header-menu","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group Header-menu" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
 <div class="wp-block-group"><!-- wp:site-logo {"width":205} /--></div>
 <!-- /wp:group -->
 
@@ -19,12 +22,41 @@
 <!-- /wp:group -->
 
 <!-- wp:group {"className":"navbar_Image_Section","layout":{"type":"flex","flexWrap":"nowrap"}} -->
-<div class="wp-block-group navbar_Image_Section"><!-- wp:image {"id":30,"width":21,"height":24,"sizeSlug":"full","linkDestination":"none"} -->
-<figure class="wp-block-image size-full is-resized"><img src="<?php echo esc_url( get_template_directory_uri() );?>/assets/images/earth-americas-solid.jpg" alt="" class="wp-image-30" width="21" height="24"/></figure>
+<div class="wp-block-group navbar_Image_Section"><!-- wp:image {"id":30,"width":21,"height":24,"sizeSlug":"full","linkDestination":"none","className":"earthicons"} -->
+<figure class="wp-block-image size-full is-resized earthicons"><img src="<?php echo esc_url( get_template_directory_uri() );?>/assets/images/earth-americas-solid.jpg" alt="" class="wp-image-30" width="21" height="24"/></figure>
 <!-- /wp:image --></div>
 <!-- /wp:group --></div>
 <!-- /wp:group --></div>
 <!-- /wp:group -->
-<!-- wp:group {"layout":{"type":"constrained"}} -->
-<div id="overlay" class="wp-block-group"></div>
+
+<!-- wp:group {"backgroundColor":"background","className":"earthicons-submenu","layout":{"type":"constrained","contentSize":"100px"}} -->
+<div class="wp-block-group earthicons-submenu has-background-background-color has-background"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group"><!-- wp:paragraph -->
+<p>English</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group"><!-- wp:paragraph -->
+<p>Dutch</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group"><!-- wp:paragraph -->
+<p>French</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group"><!-- wp:paragraph -->
+<p>Sweden</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group"><!-- wp:paragraph -->
+<p>French</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
 <!-- /wp:group -->
